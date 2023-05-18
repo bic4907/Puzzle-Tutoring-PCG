a151c2aac3e311b70393f5de2f5a571555327101
Baek In-Chang <bic4907@gmail.com>
2023-05-19 02:58:40 +0900

    .

--- Project/Assets/ML-Agents/Examples/Match3/Scripts/BoardManualAgent.cs
+++ Project/Assets/ML-Agents/Examples/Match3/Scripts/BoardManualAgent.cs
@@ -61,13 +61,22 @@ namespace Unity.MLAgentsExamples
         public List<int> ComparisonCounts;
         public bool SaveFirebaseLog = false;
 
+        private FirebaseLogger m_FirebaseLogger;
+
         protected void Awake()
         {
             Board = GetComponent<Match3Board>();
             m_ModelOverrider = GetComponent<ModelOverrider>();
             m_Logger = new PCGStepLog();
 
-            
+            if (SaveFirebaseLog)
+            {
+                // Add FirebaseLogger component in this game objct
+                if (this.gameObject.GetComponent<FirebaseLogger>() == null)
+                {
+                    m_FirebaseLogger = this.gameObject.AddComponent<FirebaseLogger>();
+                }
+            }
 
             // Parsing the augments
             if(ParameterManagerSingleton.GetInstance().HasParam("targetPlayer"))
@@ -166,7 +175,7 @@ namespace Unity.MLAgentsExamples
                 log.Time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
                 log.InstanceUUID = m_uuid;
                 log.SkillKnowledge = m_ManualSkillKnowledge;
-                FirebaseLogger.Instance.Post(log);
+                m_FirebaseLogger.Post(log);
             }
 
         }
