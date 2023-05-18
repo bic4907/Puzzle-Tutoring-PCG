9fa6f2bced5ed212ddfc24c6b10354a73f31d3f9
Baek In-Chang <bic4907@gmail.com>
2023-05-19 00:26:45 +0900

    Done: Firebase logging feature

--- Project/Assets/ML-Agents/Examples/Match3/Scripts/FirebaseLogger.cs
+++ Project/Assets/ML-Agents/Examples/Match3/Scripts/FirebaseLogger.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
+using System.Net;
 
 
 namespace Unity.MLAgentsExamples
@@ -15,7 +16,7 @@ namespace Unity.MLAgentsExamples
         private static FirebaseLogger m_Instance;
         private const string FirebaseUrl = "https://tutoringpcg-default-rtdb.firebaseio.com/gameResult/log.json";
 
-
+        private string m_ExternalIPAddress;
 
         public static FirebaseLogger Instance { 
             get
@@ -29,21 +30,21 @@ namespace Unity.MLAgentsExamples
             set { value = m_Instance; }
         }
 
-
-        public async void SendGameResult()
+        public FirebaseLogger()
         {
-            Debug.Log("Sending POST request to Firebase...");
-            await SendPostRequest();
+            FetchExternalIPAddress();
+        }
+
+        public async void Post(FirebaseLog log)
+        {
+            await SendPostRequest(log);
 
         }
 
-        private async Task SendPostRequest()
+        private async Task SendPostRequest(FirebaseLog log)
         {
-            Dictionary<string, object> jsonBody = new Dictionary<string, object>
-            {
-                { "name", "John" },
-                { "age", 30 }
-            };
+            Dictionary<string, object> jsonBody = log.ToDict();
+            jsonBody.Add("IPAddress", m_ExternalIPAddress);
 
             string jsonString = JsonConvert.SerializeObject(jsonBody);
 
@@ -51,15 +52,14 @@ namespace Unity.MLAgentsExamples
             {
                 // Create the HTTP content with JSON body
                 var content = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
-                Debug.Log(jsonString);
+                
                 // Send the POST request asynchronously
                 HttpResponseMessage response = await client.PostAsync(FirebaseUrl, content);
 
                 // Check if the request was successful
                 if (response.IsSuccessStatusCode)
                 {
-                    Debug.Log(response.Content.ReadAsStringAsync().Result);
-                    Debug.Log("POST request sent successfully.");
+                    // Debug.Log(response.Content.ReadAsStringAsync().Result);
                 }
                 else
                 {
@@ -68,6 +68,49 @@ namespace Unity.MLAgentsExamples
             }
         }
 
+        private void FetchExternalIPAddress()
+        {
+            using (var webClient = new WebClient())
+            {
+                // Make a request to the IP address detection service
+                m_ExternalIPAddress = webClient.DownloadString("https://api.ip.pe.kr/");
+
+            }
+        }
+
     }
+
+
+    public class FirebaseLog
+    {
+        public int EpisodeCount;
+        public int EpisodeStepCount;
+        public int TotalStepCount;
+        public string Time;
+        public string InstanceUUID;
+        public SkillKnowledge SkillKnowledge;
+
+        public FirebaseLog()
+        {
+           
+        }
+
+        public Dictionary<string, object> ToDict()
+        {
+            // Reigster all local variables
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("EpisodeCount", EpisodeCount);
+            dict.Add("EpisodeStepCount", EpisodeStepCount);
+            dict.Add("TotalStepCount", TotalStepCount);
+            dict.Add("Time", Time);
+            dict.Add("InstanceUUID", InstanceUUID);
+
+            dict.Add("CurrentMatches", SkillKnowledge.CurrentMatchCounts);
+            dict.Add("CurrentLearned", SkillKnowledge.ManualCheck);
+
+            return dict;
+        }
+    }
+
 
 }
